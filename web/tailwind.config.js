a99df19cc6682990f1196d8a328341dca64a9ebe
Dano913 <daniaxve913@gmail.com>
2026-04-19 19:02:59 +0200

    - Tailwind configurado y htmls diseñados con informacion estándar

---
+++ web/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./js/**/*.{html,js}",
+    "./js/components/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
